149d95abd30fd6a959cf4b449bf49574aa05eca6
Iamneo <techadmin@iamneo.ai>
2023-08-06 18:09:41 +0000

    Backup Commit

--- fb/src/test/java/com/fblogin/Assignment1.java
+++ fb/src/test/java/com/fblogin/Assignment1.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class Assignment1 {
 
